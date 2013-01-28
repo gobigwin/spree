8c51885b21295cb19f899bbb6e24919502e8ad28
John Bohn <jjbohn@gmail.com>
2013-01-28 14:34:57 -0600

    Make featured products only show up on homepage

--- core/app/controllers/spree/home_controller.rb
+++ core/app/controllers/spree/home_controller.rb
@@ -7,7 +7,7 @@ module Spree
       @searcher = Spree::Config.searcher_class.new(params)
       @searcher.current_user = try_spree_current_user
       @searcher.current_currency = current_currency
-      @products = @searcher.retrieve_products
+      @products = @searcher.retrieve_products.featured
       respond_with(@products)
     end
   end
