db3fd97f73dacf89f56a7fb72c727ca95385d044
BuckBuchanan <buck.buchanan@gmail.com>
2013-11-19 16:39:03 -0600

    Switching emails to use id instead of actual model object for order and shipments

--- core/app/mailers/spree/order_mailer.rb
+++ core/app/mailers/spree/order_mailer.rb
@@ -3,18 +3,18 @@ module Spree
     helper 'spree/base'
 
     def confirm_email(order, resend = false)
-      @order = order
+      @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
       subject = (resend ? "[#{t(:resend).upcase}] " : '')
-      subject += "#{Spree::Config[:site_name]} #{t('order_mailer.confirm_email.subject')} ##{order.number}"
-      mail(:to => order.email,
+      subject += "#{Spree::Config[:site_name]} #{t('order_mailer.confirm_email.subject')} ##{@order.number}"
+      mail(:to => @order.email,
            :subject => subject)
     end
 
     def cancel_email(order, resend = false)
-      @order = order
+      @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
       subject = (resend ? "[#{t(:resend).upcase}] " : '')
-      subject += "#{Spree::Config[:site_name]} #{t('order_mailer.cancel_email.subject')} ##{order.number}"
-      mail(:to => order.email,
+      subject += "#{Spree::Config[:site_name]} #{t('order_mailer.cancel_email.subject')} ##{@order.number}"
+      mail(:to => @order.email,
            :subject => subject)
     end
   end
