df8e1387ba5bc84a4807600d4af1cad7e628d091
BuckBuchanan <buck.buchanan@gmail.com>
2013-11-19 17:56:41 -0600

    Improving detection of object vs id

--- core/app/mailers/spree/order_mailer.rb
+++ core/app/mailers/spree/order_mailer.rb
@@ -3,7 +3,7 @@ module Spree
     helper 'spree/base'
 
     def confirm_email(order, resend = false)
-      @order = order.respond_to?(:number) ? order : Spree::Order.find(order)
+      find_order(order)
       subject = (resend ? "[#{t(:resend).upcase}] " : '')
       subject += "#{Spree::Config[:site_name]} #{t('order_mailer.confirm_email.subject')} ##{@order.number}"
       mail(:to => @order.email,
@@ -11,11 +11,16 @@ module Spree
     end
 
     def cancel_email(order, resend = false)
-      @order = order.respond_to?(:number) ? order : Spree::Order.find(order)
+      find_order(order)
       subject = (resend ? "[#{t(:resend).upcase}] " : '')
       subject += "#{Spree::Config[:site_name]} #{t('order_mailer.cancel_email.subject')} ##{@order.number}"
       mail(:to => @order.email,
            :subject => subject)
     end
+
+    private
+      def find_order(order)
+        @order = order.is_a?(Spree::Order) ? order : Spree::Order.find(order)
+      end
   end
 end
